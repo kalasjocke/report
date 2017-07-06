1fd660034cdc8fd91b797d73e9febd6a0f42ac53
Joakim Ekberg <jocke.ekberg@gmail.com>
2017-07-06 23:33:11 +0200

    Connect all the pieces

---
+++ index.js
@@ -0,0 +1,49 @@
+const moment = require('moment')
+const program = require('commander')
+const fetch = require('node-fetch')
+const Table = require('easy-table')
+
+const { transform } = require('./transform')
+const { parse } = require('./parse')
+
+function main() {
+  program
+    .version('0.1.0')
+    .usage('[options]')
+    .option('-c --calendar <uri>', 'URI to ics file')
+    .option('-s, --since <date>', 'Show entries more recent than specific date.', moment)
+    .option('-u, --until <date>', 'Show entries older than specific date.', moment)
+    .parse(process.argv)
+
+  if (program.calendar && program.since && program.until) {
+    const table = new Table
+    fetch(program.calendar).then(res => {
+      res.text().then(data => {
+        const entries = parse(data)
+        const transformed = transform(program.since, program.until, entries)
+        transformed.forEach(day => {
+          if (Object.keys(day.projects).length > 0) {
+            Object.keys(day.projects).forEach(projectName => {
+              const project = day.projects[projectName]
+              table.cell('Date', day.date.format('YYYY-MM-DD'))
+              table.cell('Project', projectName)
+              table.cell('Duration', project.duration + 'h')
+              table.cell('Description', project.descriptions.filter(d => d.trim() !== '').join(', '))
+              table.newRow()
+            })
+          } else {
+            table.cell('Date', day.date.format('YYYY-MM-DD'))
+            table.newRow()
+          }
+        })
+        console.log(table.toString())
+      })
+    })
+  } else {
+    program.help()
+  }
+}
+
+process.on('unhandledRejection', error => console.log(error))
+
+main()
